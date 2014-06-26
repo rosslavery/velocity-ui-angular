34c6ec4689f2330662b4413108e5a01a389fdf9b
Ross Lavery <rosslavery@gmail.com>
2014-06-26 16:02:14 -0400

    feat(rl-velocity): Add class-parsing for duration and easing. Fix SIAF not firing.

--- rl-velocity.js
+++ rl-velocity.js
@@ -1,6 +1,11 @@
-(function(angular) {
+(function() {
 
   var rlVelocity =  angular.module('rl-velocity', []);
+
+  var defaultOptions = {
+    duration: 250,
+    easing: 'easeInOutQuad'
+  };
 
   var animationsList = [
     'fadeIn',
@@ -67,13 +72,33 @@
     return '.velocity-' + animation.replace('.', '-');
   }
 
+  function _parseClassList(classList) {
+    var parsedOptions = {};
+
+    angular.forEach(classList, function(currClass) {
+      if (currClass.indexOf('velocity-easing-') > -1) {
+        parsedOptions.easing = currClass.split('velocity-easing-')[1];
+      }
+
+      if (currClass.indexOf('velocity-duration-') > -1) {
+        parsedOptions.duration = currClass.split('velocity-duration-')[1];
+      }
+
+    });
+
+    return parsedOptions;
+
+  }
+
   function _createVelocityAnimation(animation) {
     return function($el, done) {
 
-      // TODO: Parse $el.classList for duration, easing, other classes
-      $el.velocity(animation, {
-        complete: done
-      });
+      var parsedOptions = _parseClassList($el[0].classList);
+      var options = angular.extend(defaultOptions, parsedOptions);
+      options.display = $el.css('display');
+      options.complete = done;
+
+      $el.velocity(animation, options);
 
       return function(cancel) {
         if (cancel) {
@@ -83,12 +108,50 @@
     }
   }
 
+  function _createVelocityClassAnimation(animation) {
+    return function ($el, className, done) {
+
+      var parsedOptions = _parseClassList($el[0].classList);
+      var options = angular.extend(defaultOptions, parsedOptions);
+      options.display = $el.css('display');
+      options.complete = done;
+
+      if ('ng-hide' === className || 'ng-show' === className) {
+        $el.velocity(animation, options);
+
+        return function (cancel) {
+          if (cancel) {
+            $el.velocity('stop');
+          }
+        };
+      }
+    };
+  }
+
   function _createAngularAnimation(animation) {
+
+    var opp = animation.replace('In', 'Out');
+
+    if (opp.indexOf('Down') > -1) {
+      opp = opp.replace('Down', 'Up');
+    }
+    else if (opp.indexOf('Up') > -1) {
+      opp = opp.replace('Up', 'Down');
+    }
+    else if (opp.indexOf('Left') > -1) {
+      opp = opp.replace('Left', 'Right');
+    }
+    else if (opp.indexOf('Right') > -1) {
+      opp = opp.replace('Right', 'Left');
+    }
+
     return function() {
       return {
         enter: _createVelocityAnimation(animation),
-        leave: _createVelocityAnimation(animation),
-        move: _createVelocityAnimation(animation)
+        leave: _createVelocityAnimation(opp),
+        move: _createVelocityAnimation(animation),
+        addClass: _createVelocityClassAnimation(animation),
+        removeClass: _createVelocityClassAnimation(opp)
       }
     };
   }
@@ -96,12 +159,13 @@
   /*
     Build the animations
   */
-  var className;
-  angular.forEach(animationsList, function(animationName) {
+  var className, oppositeClassName;
+  angular.forEach(animationsList, function(animation) {
 
-    className = getClassName(animation);
-    rlVelocity.animation(className, _createAngularAnimation(animationName));
+    className = _getClassName(animation);
+
+    rlVelocity.animation(className, _createAngularAnimation(animation));
 
   });
 
-});
+})();
