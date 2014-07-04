c0dd149498ea8058cb762e1d1e9f10010db0d1a3
Ross Lavery <rosslavery@gmail.com>
2014-07-04 15:13:52 -0400

    chore(build): Improve build and release tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,64 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+          '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+          '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+          ' License: <%= pkg.license %> */\n',
     jshint: {
       options: {
         jshintrc: true
       },
-      build: ['rl-velocity.js']
+      build: ['<%= pkg.name %>.js']
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: {
+          block: true
+        }
+      },
+      build: {
+        src: ['<%= pkg.name %>.js'],
+        dest: '<%= pkg.name %>.js'
+      }
     },
     uglify: {
       options: {
-        mangle: true,
-        banner: '/*! <%= pkg.name %> - <%= pkg.author %> - <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '<%= banner %>',
+        mangle: true
       },
       build: {
-        src: 'rl-velocity.js',
-        dest: 'rl-velocity.min.js'
+        src: '<%= pkg.name %>.js',
+        dest: '<%= pkg.name %>.min.js'
+      }
+    },
+    bump: {
+      options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        commit: true,
+        commitMessage: 'chore(release): %VERSION%',
+        commitFiles: ['.'],
+        createTag: true,
+        tagName: '%VERSION%',
+        tagMessage: 'Release %VERSION%',
+        push: false,
+        // pushTo: 'upstream',
+        // gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d'
       }
     }
   });
 
-  grunt.registerTask('build', ['jshint:build', 'uglify:build']);
+
+  grunt.registerTask("release", "Release a new version, push it and publish it", function(target) {
+    if (!target) {
+      target = "patch";
+    }
+    return grunt.task.run("bump-only:" + target, "build", "bump-commit");
+  });
+  grunt.registerTask('build', ['jshint:build', 'concat:build', 'uglify:build']);
   grunt.registerTask('default', ['build']);
 
 };
