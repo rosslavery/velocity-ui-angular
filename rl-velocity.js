c4382cfc14f6bffde1a9fcf16419361db91ba4ca
Ross Lavery <rosslavery@gmail.com>
2014-07-10 14:26:11 -0400

    chore(release): 0.3.1

--- rl-velocity.js
+++ rl-velocity.js
@@ -1,4 +1,4 @@
-/*! rl-velocity - v0.3.0 - 2014-07-08
+/*! rl-velocity - v0.3.1 - 2014-07-10
 * https://github.com/rosslavery/rl-velocity
 * Copyright (c) 2014 Ross Lavery <rosslavery@gmail.com>; License: MIT */
 (function(angular) {
